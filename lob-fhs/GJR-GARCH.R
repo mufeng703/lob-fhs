3e0f17a3b5f08884b04241f96612475c6f5a8f24
jcratebo703 <jcratebo703@gmail.com>
2021-01-02 00:42:38 +0800

    test rugarch

--- lob-fhs/GJR-GARCH.R
+++ lob-fhs/GJR-GARCH.R
@@ -7,5 +7,11 @@ data(sp500ret)
 # create a cluster object to be used as part of this demonstration
 cluster = makePSOCKcluster(15)
 
-plot(sp500ret$SP500RET)
+# start with default GARCH spec.
+spec = ugarchspec() #the empty function specifies the default model. 
+print(spec)
 
+#spec is a basic ARIMA(1,1),GARCH(1,1) model.
+#use data to fit it
+def.fit = ugarchfit(spec = spec, data = sp500ret)
+print(def.fit)
